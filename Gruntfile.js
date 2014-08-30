584238013605bd5cd2f7081c4f6de927aa368359
Emil van Galen <emil.van.galen@gmail.com>
2014-08-30 09:13:21 +0200

    Fixes to (hopefully) get TravisCI working for this repo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,8 @@
 module.exports = function(grunt) {
     'use strict';
 
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    //grunt plugins
+    require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
